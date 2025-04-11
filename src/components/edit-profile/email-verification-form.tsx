683ab50b12257c9fdaedb324d481004ceee35c21
SLIMANI Imed Eddine Abderrahmane <imeds2000@gmail.com>
2025-04-11 23:05:07 +0100

    update readme

--- src/components/edit-profile/email-verification-form.tsx
+++ src/components/edit-profile/email-verification-form.tsx
@@ -106,8 +106,11 @@ export function EmailVerificationForm({
             </div>
             <div className="mt-2">
               <input
-                type="password"
+                type="text"
                 disabled={!!email_verified_at}
+                placeholder={
+                  !!email_verified_at ? 'Email is already verified' : ''
+                }
                 className="block w-full rounded-md bg-white px-3 py-1.5 text-base text-gray-900 outline outline-1 -outline-offset-1 outline-gray-300 placeholder:text-gray-400 focus:outline focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-600 sm:text-sm/6 disabled:cursor-not-allowed disabled:bg-gray-50 disabled:text-gray-500 disabled:ring-gray-200"
                 {...register('code')}
               />
