f5e787e2e874b3e6f81d8e68d338bc6a41165a54
SLIMANI Imed Eddine Abderrahmane <imeds2000@gmail.com>
2025-06-26 20:19:46 +0100

    add ecosystem.config.js file and edit next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -3,16 +3,10 @@ import type { NextConfig } from 'next'
 const nextConfig: NextConfig = {
   images: {
     remotePatterns: [
-      {
-        protocol: 'https',
-        hostname: 'images.unsplash.com',
-      },
-      {
-        protocol: 'http',
-        hostname: 'localhost',
-        port: '8000',
-        pathname: '/storage/**',
-      },
+      new URL(
+        'https://arthive-be-h9ekercmcwb9ffdt.francecentral-01.azurewebsites.net/storage/**'
+      ),
+      new URL('http://localhost:8000/storage/**'),
     ],
   },
 }
