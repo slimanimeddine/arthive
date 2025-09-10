a89de2a649867ae220e812309f170b14f82044cf
Imed Eddine Abderrahmane SLIMANI <imeds2000@gmail.com>
2025-09-10 11:14:55 +0100

    update packages and next config

---
+++ next.config.js
@@ -0,0 +1,17 @@
+import { fileURLToPath } from "node:url";
+import { createJiti } from "jiti";
+const jiti = createJiti(fileURLToPath(import.meta.url));
+
+await jiti.import("./src/env/client.ts");
+await jiti.import("./src/env/server.ts");
+
+/** @type {import('next').NextConfig} */
+export default {
+  typedRoutes: true,
+  images: {
+    remotePatterns: [
+      new URL("http://localhost:8000/storage/**"),
+      new URL("https://api.arthive.foo/storage/**"),
+    ],
+  },
+};
